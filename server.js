f1f34271185506c578a17267862cc4ede17e8a3e
Jake West <jacobkwest93@gmail.com>
2020-09-05 17:05:30 -0600

    Add flower data

--- server.js
+++ server.js
@@ -3,7 +3,38 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Flower Shop'
-
+app.locals.flowers = [
+  {
+    id: 1,
+    name: 'Rose',
+    image: 'https://images.freeimages.com/images/large-previews/4dd/rose-1514960.jpg',
+    quantity: {
+      amount: 4,
+      metric: 'Dozen'
+    },
+    price: 25,
+  },
+  {
+    id: 2,
+    name: 'Tulip',
+    image: 'https://images.freeimages.com/images/large-previews/e48/tulip-1401330.jpg',
+    quantity: {
+      amount: 8,
+      metric: 'Pack of 15 (assorted colors)'
+    },
+    price: 35
+  },
+  {
+    id: 3,
+    name: 'Orchid',
+    image: 'https://images.freeimages.com/images/large-previews/d0f/orchid-1363254.jpg',
+    quantity: {
+      amount: 12,
+      metric: 'Double Stem Plants'
+    },
+    price: 45
+  }
+]
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}`)
